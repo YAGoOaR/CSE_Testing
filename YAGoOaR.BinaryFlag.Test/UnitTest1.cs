6a20ccca86f8b21661623b03481d9db1f1bc3010
YAGoOaR <yegorgribenko68@gmail.com>
2021-06-06 15:58:30 +0300

    Add limit tests for length parameter

--- YAGoOaR.BinaryFlag.Test/UnitTest1.cs
+++ YAGoOaR.BinaryFlag.Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IIG.BinaryFlag;
 
@@ -6,18 +7,74 @@ namespace YAGoOaR.BinaryFlag.Test
     [TestClass]
     public class BinaryFlagTests
     {
+        const ulong maxBinaries = 17179868704;
+        const ulong minBinaries = 2;
+        const ulong middlePoint = 8589934353;
+        const ulong offPointOffset = 1;
+        const ulong outPointOffset = 1;
         const ulong inPoint1 = 10;
 
-        [TestMethod]
-        public void Test_initialValue_True() {
-            MultipleBinaryFlag flag = new MultipleBinaryFlag(inPoint1, true);
-            Assert.AreEqual(flag.GetFlag(), true);
-        }
+        [TestClass]
+        public class initialValueTests
+        {
+            [TestMethod]
+            public void Test_initialValue_True() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(inPoint1, true);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
 
-        [TestMethod]
-        public void Test_initialValue_False() {
-            MultipleBinaryFlag flag = new MultipleBinaryFlag(inPoint1, false);
-            Assert.AreEqual(flag.GetFlag(), false);
+            [TestMethod]
+            public void Test_initialValue_False() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(inPoint1, false);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+        }
+        [TestClass]
+        public class LengthLimitTests
+        {
+            [TestMethod]
+            public void Test_InPoint_middle() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(middlePoint);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(minBinaries);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(maxBinaries);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(minBinaries + offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(maxBinaries - offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_min_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(minBinaries - outPointOffset);
+                });
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_max_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(maxBinaries + outPointOffset);
+                });
+            }
         }
     }
 }
