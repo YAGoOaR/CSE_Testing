36489f0e792ab5c8aaac1e717a8f6dddef86c122
YAGoOaR <yegorgribenko68@gmail.com>
2021-06-06 17:43:48 +0300

    Add tests for SetFlag, ResetFlag and Dispose methods

--- YAGoOaR.BinaryFlag.Test/UnitTest1.cs
+++ YAGoOaR.BinaryFlag.Test/UnitTest1.cs
@@ -14,6 +14,11 @@ namespace YAGoOaR.BinaryFlag.Test
         const ulong outPointOffset = 1;
         const ulong inPoint1 = 10;
 
+        const ulong range1_length = 10;
+        const ulong range1_max = range1_length - 1;
+        const ulong range1_min = 0;
+        const ulong range1_mid = 5;
+
         [TestClass]
         public class initialValueTests
         {
@@ -29,8 +34,28 @@ namespace YAGoOaR.BinaryFlag.Test
                 Assert.AreEqual(flags.GetFlag(), false);
             }
         }
+
         [TestClass]
-        public class LengthLimitTests
+        public class GeneralTests
+        {
+            [TestMethod]
+            public void Test_ResetFlag() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length, true);
+                flags.ResetFlag(range1_mid);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_SetFlag() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(2, false);
+                flags.SetFlag(0);
+                flags.SetFlag(1);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+        }
+
+        [TestClass]
+        public class LengthParamLimitTests
         {
             [TestMethod]
             public void Test_InPoint_middle() {
@@ -74,6 +99,143 @@ namespace YAGoOaR.BinaryFlag.Test
                 Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
                     MultipleBinaryFlag flags = new MultipleBinaryFlag(maxBinaries + outPointOffset);
                 });
+            }
+        }
+
+        [TestClass]
+        public class ResetFlagLimitTests
+        {
+            [TestMethod]
+            public void Test_InPoint_middle() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.ResetFlag(range1_mid);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.ResetFlag(range1_min);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.ResetFlag(range1_max);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.ResetFlag(range1_min + offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.ResetFlag(range1_max - offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), false);
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_min_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                    unchecked {
+                        flags.ResetFlag(range1_min - outPointOffset);
+                    }
+                });
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_max_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                    unchecked {
+                        flags.ResetFlag(range1_max + outPointOffset);
+                    }
+                });
+            }
+        }
+
+        [TestClass]
+        public class SetFlagLimitTests
+        {
+            [TestMethod]
+            public void Test_InPoint_middle() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.SetFlag(range1_mid);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.SetFlag(range1_min);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OnPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.SetFlag(range1_max);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_min() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.SetFlag(range1_min + offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OffPoint_max() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                flags.SetFlag(range1_max - offPointOffset);
+                Assert.AreEqual(flags.GetFlag(), true);
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_min_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                    unchecked {
+                        flags.SetFlag(range1_min - outPointOffset);
+                    }
+                });
+            }
+
+            [TestMethod]
+            public void Test_OutPoint_max_Throw() {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                    MultipleBinaryFlag flags = new MultipleBinaryFlag(range1_length);
+                    unchecked {
+                        flags.SetFlag(range1_max + outPointOffset);
+                    }
+                });
+            }
+        }
+
+        [TestClass]
+        public class DisposedFlagStateTests
+        {
+            [TestMethod]
+            public void Dispose_GetFlag_Expect_Null() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(inPoint1);
+                flags.Dispose();
+                Assert.AreEqual(flags.GetFlag(), null);
+            }
+            [TestMethod]
+            public void Dispose_Setflag_GetFlag_Expect_Null() {
+                MultipleBinaryFlag flags = new MultipleBinaryFlag(inPoint1);
+                flags.Dispose();
+                flags.ResetFlag(0);
+                flags.SetFlag(1);
+                Assert.AreEqual(flags.GetFlag(), null);
             }
         }
     }
