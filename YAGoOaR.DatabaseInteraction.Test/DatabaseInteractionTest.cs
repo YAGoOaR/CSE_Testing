661f95761155af3a57db6beca03a34df419e4a1c
YAGoOaR <yegorgribenko68@gmail.com>
2021-06-11 19:15:50 +0300

    Add tests for flags and file worker

--- YAGoOaR.DatabaseInteraction.Test/DatabaseInteractionTest.cs
+++ YAGoOaR.DatabaseInteraction.Test/DatabaseInteractionTest.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IIG.CoSFE.DatabaseUtils;
 using IIG.PasswordHashingUtils;
+using IIG.BinaryFlag;
+using IIG.FileWorker;
+using System.IO;
 
 namespace YAGoOaR.DatabaseInteraction.Test
 {
     [TestClass]
-    public class DatabaseInteractionTest
+    public class TestAuthDB
     {
         const string Server = @"YAGOOAR-DESKTOP\MSSQLSERVER1";
         const bool IsTrusted = false;
@@ -13,95 +16,171 @@ namespace YAGoOaR.DatabaseInteraction.Test
         const string Password = @"aVerySecurePassword";
         const int ConnectionTimeout = 75;
 
-        [TestClass]
-        public class TestAuthDB
-        {
-            const string Database = @"IIG.CoSWE.AuthDB";
-            const string salt = "just a salt";
-            const uint adlerMod32 = 54321;
-            AuthDatabaseUtils DB;
+        const string Database = @"IIG.CoSWE.AuthDB";
+        const string salt = "just a salt";
+        const uint adlerMod32 = 54321;
+        AuthDatabaseUtils DB;
 
-            public void ClearDB() {
-                DB.ExecSql("DELETE FROM Credentials");
-            }
+        public void ClearDB() {
+            DB.ExecSql("DELETE FROM Credentials");
+        }
 
-            [TestInitialize]
-            public void TestInit() {
-                DB = new AuthDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
-                ClearDB();
-            }
+        [TestInitialize]
+        public void TestInit() {
+            DB = new AuthDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
+            ClearDB();
+        }
 
-            [TestMethod]
-            public void Test_Check_Credentials() {
-                string login = "user1";
-                string password = "abcd";
-                string message = $"login: {login}, password: {password}";
-                string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
+        [TestCleanup]
+        public void Cleanup() {
+            ClearDB();
+        }
 
-                Assert.IsTrue(DB.AddCredentials(login, passHash),
-                    $"AddCredentials failed." + message);
+        [TestMethod]
+        public void Test_Check_Credentials() {
+            string login = "user1";
+            string password = "abcd";
+            string message = $"login: {login}, password: {password}";
+            string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
 
-                Assert.IsTrue(DB.CheckCredentials(login, passHash),
-                    $"CheckCredentials failed." + message);
-            }
+            Assert.IsTrue(DB.AddCredentials(login, passHash),
+                $"AddCredentials failed." + message);
 
-            [TestMethod]
-            public void Test_Delete_Credentials() {
-                string login = "user2";
-                string password = "qwerty";
-                string message = $"login: {login}, password: {password}";
-                string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
+            Assert.IsTrue(DB.CheckCredentials(login, passHash),
+                $"CheckCredentials failed." + message);
+        }
 
-                Assert.IsTrue(DB.AddCredentials(login, passHash),
-                    $"AddCredentials failed." + message);
+        [TestMethod]
+        public void Test_Delete_Credentials() {
+            string login = "user2";
+            string password = "qwerty";
+            string message = $"login: {login}, password: {password}";
+            string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
 
-                Assert.IsTrue(DB.DeleteCredentials(login, passHash),
-                    $"DeleteCredentials failed." + message);
+            Assert.IsTrue(DB.AddCredentials(login, passHash),
+                $"AddCredentials failed." + message);
 
-                Assert.IsFalse(DB.CheckCredentials(login, passHash),
-                    $"CheckCredentials failed." + message);
-            }
+            Assert.IsTrue(DB.DeleteCredentials(login, passHash),
+                $"DeleteCredentials failed." + message);
 
-            [TestMethod]
-            public void Test_Add_Existing_Credentials() {
-                string login = "user2";
-                string password = "qwerty";
-                string message = $"login: {login}, password: {password}";
-                string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
+            Assert.IsFalse(DB.CheckCredentials(login, passHash),
+                $"CheckCredentials failed." + message);
+        }
 
-                Assert.IsTrue(DB.AddCredentials(login, passHash),
-                    $"AddCredentials failed." + message);
-                Assert.IsFalse(DB.AddCredentials(login, passHash),
-                    $"AddCredentials(existing) result is True. Expected: False. " + message);
-            }
+        [TestMethod]
+        public void Test_Add_Existing_Credentials() {
+            string login = "user2";
+            string password = "qwerty";
+            string message = $"login: {login}, password: {password}";
+            string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
 
-            [DataTestMethod]
-            [DataRow("Valid data", "user1", "abcd", true)]
-            [DataRow("Unicode data", "█", "□", true)]
-            [DataRow("Null data", null, null, false)]
-            [DataRow("Valid login, null password", "login", null, false)]
-            [DataRow("Null login, valid password", null, "password", false)]
-            [DataRow("Empty data", "", "", false)]
-            [DataRow("Valid login, empty password", "login", "", true)]
-            [DataRow("Empty login, valid password", "", "password", false)]
-            public void Test_Valid_Invalid_Credentials(string testName, string login, string password, bool isValid) {
-                string message = $"Test name: {testName}. login: {login}, password: {password}";
-                string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
+            Assert.IsTrue(DB.AddCredentials(login, passHash),
+                $"AddCredentials failed." + message);
+            Assert.IsFalse(DB.AddCredentials(login, passHash),
+                $"AddCredentials(existing) result is True. Expected: False. " + message);
+        }
 
-                Assert.AreEqual(isValid, DB.AddCredentials(login, passHash),
-                    $"AddCredentials result is {!isValid}. Expected: {isValid} " + message);
+        [DataTestMethod]
+        [DataRow("Valid data", "user1", "abcd", true)]
+        [DataRow("Unicode data", "█", "□", true)]
+        [DataRow("Null data", null, null, false)]
+        [DataRow("Valid login, null password", "login", null, false)]
+        [DataRow("Null login, valid password", null, "password", false)]
+        [DataRow("Empty data", "", "", false)]
+        [DataRow("Valid login, empty password", "login", "", true)]
+        [DataRow("Empty login, valid password", "", "password", false)]
+        public void Test_Valid_Invalid_Credentials(string testName, string login, string password, bool isValid) {
+            string message = $"Test name: {testName}. login: {login}, password: {password}";
+            string passHash = PasswordHasher.GetHash(password, salt, adlerMod32);
 
-                Assert.AreEqual(isValid, DB.CheckCredentials(login, passHash),
-                    $"CheckCredentials result is {!isValid}. Expected: {isValid} " + message);
+            Assert.AreEqual(isValid, DB.AddCredentials(login, passHash),
+                $"AddCredentials result is {!isValid}. Expected: {isValid} " + message);
 
-                Assert.AreEqual(isValid, DB.DeleteCredentials(login, passHash),
-                    $"DeleteCredentials result is {!isValid}. Expected: {isValid} " + message);
-            }
+            Assert.AreEqual(isValid, DB.CheckCredentials(login, passHash),
+                $"CheckCredentials result is {!isValid}. Expected: {isValid} " + message);
 
-            [TestCleanup]
-            public void Cleanup() {
-                ClearDB();
-            }
+            Assert.AreEqual(isValid, DB.DeleteCredentials(login, passHash),
+                $"DeleteCredentials result is {!isValid}. Expected: {isValid} " + message);
+        }
+    }
+
+    [TestClass]
+    public class Test_BinaryFlag_FileWorker
+    {
+        const string outputFile = "output.txt";
+
+        [TestInitialize]
+        public void TestInit() {
+            
+        }
+
+        [TestCleanup]
+        public void Cleanup() {
+            File.Delete(outputFile);
+        }
+
+        [TestMethod]
+        public void Test_Write() {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(10);
+            Assert.IsTrue(BaseFileWorker.Write(flags.GetFlag().ToString(), outputFile));
+        }
+
+        [TestMethod]
+        public void Test_Read_True_Flag() {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(10);
+            string res = flags.GetFlag().ToString();
+            Assert.IsTrue(BaseFileWorker.Write(res, outputFile), "Write failed");
+            Assert.AreEqual(res, BaseFileWorker.ReadAll(outputFile), "Results did not match");
+        }
+
+        [TestMethod]
+        public void Test_False_Flag() {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(10, false);
+
+            string res = flags.GetFlag().ToString();
+            Assert.IsTrue(BaseFileWorker.Write(res, outputFile), "Write failed");
+            Assert.AreEqual(res, BaseFileWorker.ReadAll(outputFile), "Results did not match");
+        }
+
+        [TestMethod]
+        public void Test_Null_Flag() {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(10, false);
+            flags.Dispose();
+
+            string res = flags.GetFlag().ToString();
+            Assert.IsTrue(BaseFileWorker.Write(res, outputFile), "Write failed");
+            Assert.AreEqual(res, BaseFileWorker.ReadAll(outputFile), "Results did not match");
+        }
+
+        [TestMethod]
+        public void Test_Set_Flags() {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(3, false);
+            flags.SetFlag(0);
+            flags.SetFlag(1);
+            flags.SetFlag(2);
+            bool? res = flags.GetFlag();
+            Assert.IsTrue(res ?? false, "Unexpected GetFlag result");
+            Assert.IsTrue(BaseFileWorker.Write(res.ToString(), outputFile), "Write failed");
+            Assert.AreEqual(res.ToString(), BaseFileWorker.ReadAll(outputFile), "Results did not match");
+        }
+
+        [DataTestMethod]
+        [DataRow("output12345.txt", true)]
+        [DataRow("output12345", true)]
+        [DataRow("output12345.jpg", true)]
+        [DataRow("►◄↕‼¶§.jpg", true)]
+        [DataRow("looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+            "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+            "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.txt", false)] // Length: 257
+        [DataRow("!@#$%^&*()|=-<>?/':;.", false)]
+        public void Test_FileNames(string fileName, bool expectSuccess) {
+            MultipleBinaryFlag flags = new MultipleBinaryFlag(10);
+            string res = flags.GetFlag().ToString();
+            bool actual = BaseFileWorker.Write(res, fileName);
+            Assert.AreEqual(expectSuccess, actual, "Write failed");
+            if (!actual) return;
+            Assert.AreEqual(res, BaseFileWorker.ReadAll(fileName), "Results did not match");
+            File.Delete(fileName);
         }
     }
 }
