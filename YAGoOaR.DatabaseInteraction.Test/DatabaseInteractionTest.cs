0d74e8d33665b62e15e4f2cc99ae39f064fbb492
YAGoOaR <yegorgribenko68@gmail.com>
2021-06-11 13:36:38 +0300

    Initial commit for Lab4

---
+++ YAGoOaR.DatabaseInteraction.Test/DatabaseInteractionTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YAGoOaR.DatabaseInteraction.Test
+{
+    [TestClass]
+    public class DatabaseInteractionTest
+    {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
